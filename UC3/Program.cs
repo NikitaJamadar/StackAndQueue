67e96c52db75407cb4891592ceb9148bd07e9652
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 15:11:06 +0530

    creating Queue

---
+++ UC3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack and Queue");
+            Queues queue = new Queues();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+        }
+    }
+}
---
+++ UC3/Queues.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3
+{
+    class Queues
+    {
+        public Node top;
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                Node temp = top;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
