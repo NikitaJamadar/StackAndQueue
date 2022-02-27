b5fcd6f05ed4e398c68637ebeb9949ecc1feba4f
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 15:07:39 +0530

    Creating stack

---
+++ UC1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack program");
+            Stacks stack = new Stacks();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+
+        }
+    }
+}
---
+++ UC1/Stacks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1
+{
+    class Stacks
+    {
+        public Node top;
+        
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                newNode.next = null;
+               
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+        }
+
+       
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data+" ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
