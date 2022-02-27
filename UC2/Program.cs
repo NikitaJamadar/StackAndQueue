8ffa527acb5183e8a08560ab0359ffa03e9579e9
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 15:09:59 +0530

    Peak and pop

---
+++ UC2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wecome to stack and Queue");
+            Stacks stack = new Stacks();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(50);
+            Console.WriteLine();
+
+            stack.Display();
+
+            stack.PeakAndPop();
+
+            Console.WriteLine();
+            stack.Display();
+        }
+    }
+}
---
+++ UC2/Stacks.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2
+{
+    class Stacks
+    {
+        public Node top;
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                newNode.next = null;
+               
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+        public void PeakAndPop()
+        {
+            while (top != null)
+            {
+                Peak();
+                Pop();
+            }
+        }
+        public void Peak()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty.");
+            }
+            else
+            {
+                Console.WriteLine("The peak value in stack:"+top.data);
+            }
+        }
+        
+        public void Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("The stack is empty");
+            }
+            else
+            {
+                Console.WriteLine(top.data+" popped out in stack" );
+                top = top.next;
+            }
+        }
+       
+  
+    }
+}
